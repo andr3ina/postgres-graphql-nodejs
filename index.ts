36c0985d8b32d69f2744991417511db40e3ac7d4
Andreina Friedman <andri_sira@hotmail.com>
2022-10-27 13:53:13 -0700

    typescript

--- index.ts
+++ index.ts
@@ -3,37 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.post.create({
-    data: {
-      title: "test post",
-      content: "adding some test content",
-      authorId: 1,
-    },
-  });
-  //   await prisma.user.create({
-  //     data: {
-  //       name: "Alice",
-  //       email: "alice@prisma.io",
-  //       posts: {
-  //         create: { title: "Hello World" },
-  //       },
-  //       profile: {
-  //         create: { bio: "I like turtles" },
-  //       },
-  //     },
-  //   });
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  });
-  console.dir(allUsers, { depth: null });
-  const allposts = await prisma.post.findMany({});
-  console.dir(allposts, { depth: null });
-  const allprofiles = await prisma.profile.findMany({});
-  console.dir(allprofiles, { depth: null });
+  // ... you will write your Prisma Client queries here
 }
 
 main()
@@ -45,3 +15,50 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   await prisma.post.create({
+//     data: {
+//       title: "test post",
+//       content: "adding some test content",
+//       authorId: 1,
+//     },
+//   });
+//   //   await prisma.user.create({
+//   //     data: {
+//   //       name: "Alice",
+//   //       email: "alice@prisma.io",
+//   //       posts: {
+//   //         create: { title: "Hello World" },
+//   //       },
+//   //       profile: {
+//   //         create: { bio: "I like turtles" },
+//   //       },
+//   //     },
+//   //   });
+
+//   const allUsers = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//       profile: true,
+//     },
+//   });
+//   console.dir(allUsers, { depth: null });
+//   const allposts = await prisma.post.findMany({});
+//   console.dir(allposts, { depth: null });
+//   const allprofiles = await prisma.profile.findMany({});
+//   console.dir(allprofiles, { depth: null });
+// }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
